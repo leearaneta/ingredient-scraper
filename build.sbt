fe40912a9a40134abf39bae9dfd8859136775808
leearaneta <lee.araneta@gmail.com>
2018-01-21 12:48:39 -0500

    future stuff!!

--- build.sbt
+++ build.sbt
@@ -10,5 +10,6 @@ libraryDependencies ++= Seq(
   "org.typelevel" %% "cats-core" % "1.0.0",
   "org.jsoup" % "jsoup" % "1.8.3",
   "org.json4s" %% "json4s-native" % "3.6.0-M2",
-  "com.softwaremill.sttp" %% "core" % "1.1.4"
+  "com.softwaremill.sttp" %% "core" % "1.1.4",
+  "com.softwaremill.sttp" %% "async-http-client-backend-future" % "1.1.4"
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,13 @@
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
+import scala.concurrent.Future
 import scala.collection.JavaConverters._
 import sys.process._
 import org.json4s._
 import org.json4s.native.JsonMethods._
 import com.softwaremill.sttp._
+import com.softwaremill.sttp.asynchttpclient.future.AsyncHttpClientFutureBackend
 
 case class Ingredient(name: String, unit: Option[String], qty: Option[String])
 
@@ -23,7 +25,7 @@ object Main extends App {
     val path = "python ../ingredient-phrase-tagger/bin/"
     val parserFile = path + "parse-ingredients.py"
     val converterFile = path + "convert-to-json.py"
-    Seq("echo", s) #| parserFile #| converterFile !!
+    Seq("echo", s) #| parserFile #| converterFile !! // wrap this in IO monad or something
   }
 
   def format(s: String): List[Ingredient] = parse(s).extract[List[Ingredient]]
@@ -37,13 +39,12 @@ object Main extends App {
     valid.length / l.length.toFloat >= .75 // some arbitrary number
   }
 
-  def isIngredient(i: Ingredient): Boolean = {
-    implicit val backend = HttpURLConnectionBackend()
+  def makeRequest(i: Ingredient): Future[Response[String]] = {
+    implicit val sttpBackend = AsyncHttpClientFutureBackend()
     val appID = "5c3b30f0" // hide these
     val appKey = "6e4e5e4c702f73d47f0f1cd6937b225b"
     val requestURL = uri"https://api.edamam.com/api/food-database/parser?ingr=${i.name}&app_id=$appID&app_key=$appKey"
-    val request = sttp.get(requestURL)
-    true
+    sttp.get(requestURL).send()
   }
 
   val url = "http://allrecipes.com/recipe/9027/kung-pao-chicken/"
@@ -60,4 +61,16 @@ object Main extends App {
     .map(format) // convert to ingredient case class
     .filter(isIngredientList) // take out most things that aren't ingredients
     .flatten
+    .groupBy {
+      case Ingredient(_, None, None) => "a"
+      case _ => "b"
+    }
+    .map {
+      case ("a", sketchyIngredients) => {
+        val futures: Future[List[Response[String]]] = Future.traverse(sketchyIngredients)(makeRequest)
+        // somehow extract the responses
+      } // return a new list of nonSketchyIngredients
+      case _ => "bye"
+    }
+
 }
